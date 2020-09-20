c7b522a371a30072c1c5ad2850097c803e724c48
r2rendon <arturo@sube.la>
2020-09-20 17:33:14 -0600

    Added bot login and initial message testing

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const discord = require('discord.js')
 const bot = new discord.Client()
+require('dotenv').config()
+
+const token = process.env.BOT_TOKEN;
+
+bot.login(token);
+
+bot.on('ready', () => {
+    console.log('Connected to Monkeys Bot!');
+})
+
+bot.on('message', msg => {
+    if(msg.content === 'MSG'){
+        msg.reply('HELLO FRIEND!');
+    }
+    if (msg.content === 'pto'){
+        msg.reply('pto vos');
+    }
+})
