13a46a5a03ae4ff0cb6019c66484a90dde073268
r2rendon <arturo@sube.la>
2020-09-20 17:49:37 -0600

    Added ping, name, and info commands

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bot = new discord.Client()
 require('dotenv').config()
 
 const token = process.env.BOT_TOKEN;
+const prefix = '?';
+const version = 'v1.1';
 
 bot.login(token);
 
@@ -10,11 +12,27 @@ bot.on('ready', () => {
     console.log('Connected to Monkeys Bot!');
 })
 
-bot.on('message', msg => {
-    if(msg.content === 'MSG'){
-        msg.reply('HELLO FRIEND!');
-    }
-    if (msg.content === 'pto'){
-        msg.reply('pto vos');
+bot.on('message', message => {
+    let args = message.content.substring(prefix.length).split(" ");
+    switch (args[0]) {
+        case 'ping':
+            message.reply('pong :ping_pong:');
+            break;
+        
+        case 'name':
+            message.channel.send('Elmer Homero :sunglasses:');
+            break;
+        
+        case 'info':
+            switch(args[1]){
+                case 'author':
+                    message.channel.send('Arturo Rendon');
+                    break;
+                
+                case 'version':
+                    message.channel.send(version);
+                    break;
+            }
+            break;
     }
 })
